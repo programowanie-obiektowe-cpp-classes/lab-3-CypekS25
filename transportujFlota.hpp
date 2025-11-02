ca35163c189fd9156d19681687f3c91fa52bd19e
CypekS25 <CypSto@gmail.com>
2025-11-02 21:46:52 +0100

    solved

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,22 @@
 #pragma once
-
 #include "Stocznia.hpp"
 
-unsigned int transportujFlota(unsigned int towar)
+unsigned int transportujFlota(unsigned int towar) 
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0) return 0;
+
+    Stocznia stocznia{}; 
+    unsigned int iloscZaglowcow = 0; 
+
+    // Dopóki ³¹czna pojemnoœæ utworzonych statków jest mniejsza ni¿ wymagany towar
+    while (Stocznia::getTotalCap() < towar) {
+        Statek* nowyStatek = stocznia(); 
+        nowyStatek->transportuj(); 
+
+        // SprawdŸ, czy utworzony statek by³ ¿aglowcem 
+        if (dynamic_cast< Zaglowiec* >(nowyStatek))
+            ++iloscZaglowcow;
+        delete nowyStatek; 
+    }
+    return iloscZaglowcow; 
 }
